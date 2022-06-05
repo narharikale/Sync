290d2794cfeb27bb1fcdd40ed2fff1a233d59af9
narharikale <naraharikale5051@gmail.com>
2022-06-05 21:20:41 +0530

    added icon

---
+++ src/frontend/components/icons/Verticalmore.js
@@ -0,0 +1,9 @@
+export function VerticalmoreIcon () {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12,7a2,2,0,1,0-2-2A2,2,0,0,0,12,7Zm0,10a2,2,0,1,0,2,2A2,2,0,0,0,12,17Zm0-7a2,2,0,1,0,2,2A2,2,0,0,0,12,10Z">
+            </path>
+        </svg>
+    )
+} 
+
