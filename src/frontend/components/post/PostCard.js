98417a70fa0616346e89ed5d5036f4ecb2132fbe
narharikale <naraharikale5051@gmail.com>
2022-06-07 16:00:18 +0530

    added `timeformater` utility

--- src/frontend/components/post/PostCard.js
+++ src/frontend/components/post/PostCard.js
@@ -13,49 +13,62 @@ import {
   MenuList,
   MenuItem,
 } from '@chakra-ui/react';
+import { timeFormatter } from '../../utlis/timeFormatter';
 import { CommentIcon } from '../icons/Comment';
 import { LikeIcon } from '../icons/Like';
 import { VerticalmoreIcon } from '../icons/Verticalmore';
 
- function PostCard() {
+
+
+
+function PostCard( { post } ) {
+
+  const { firstName , lastName , avatarURL , createdAt , content , likes } = post 
   return (
     <Center>
       <Box
         h='100%'
         w={'full'}
-        bg={useColorModeValue('white', 'gray.900')}
+        bg={'white'}
         p={8}
-        borderBottom={'1px'}
-        borderBottomColor={'gray.200'}
+        // borderBottom={'1px'}
+        // borderBottomColor={'gray.200'}
         overflow={'hidden'}>
         
-        <Stack direction={'row'} spacing={4} align={'start'}>
-          <Avatar
-            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
-            alt={'Author'}
-          />
-          <Box direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={'gray.500'}> 9 minutes ago</Text>
-            <Box mt={2} > Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dignissimos blanditiis, ducimus saepe ad, delectus fugit animi cum perspiciatis commodi praesentium atque hic quos aliquid molestias? Recusandae odio quaerat sunt! </Box>
-            <Flex mt={4} gap={5}> <CommentIcon/>  <LikeIcon/> </Flex>
-          </Box>
-            <Box>
-              <Menu>
-                <MenuButton 
-                as={Button}
-                size='sm' 
-                borderColor={'gray.300'} 
-                bg={'white'} 
-                > 
-                <VerticalmoreIcon/> 
-                </MenuButton>
-                <MenuList border='1px solid black' boxShadow='3px 3px 0px #212121' p='4px' rounded='4px' bg='white' alignItems={'center'}>
-                  <MenuItem display="flex" gap='2px'> Edit</MenuItem>
-                  <MenuItem display="flex" gap='2px'> Delete</MenuItem>
-                </MenuList>
-              </Menu>
+        <Stack direction={'row'}  align={'start'} justifyContent={'space-between'} w={'100%'}>
+          <Stack direction={'row'} spacing={4} >
+            <Avatar
+              src={avatarURL}
+              alt={'Author'}
+            />
+            <Box direction={'column'} spacing={0} fontSize={'sm'}>
+              <Text fontWeight={600}>{firstName} {lastName}</Text>
+              <Text color={'gray.500'}> { timeFormatter(Date.parse(createdAt)) } ago </Text>
+              <Box mt={2}>{ content }</Box>
+              <Flex mt={4} gap={5}> 
+                <CommentIcon/>  
+                <Flex gap={1}>
+                  <LikeIcon/> {likes.likeCount}
+                </Flex> 
+              </Flex>
             </Box>
+          </Stack>
+          <Box>
+            <Menu>
+              <MenuButton 
+              as={Button}
+              size='sm' 
+              borderColor={'gray.300'} 
+              bg={'white'} 
+              > 
+              <VerticalmoreIcon/> 
+              </MenuButton>
+              <MenuList border='1px solid black' boxShadow='3px 3px 0px #212121' p='4px' rounded='4px' bg='white' alignItems={'center'}>
+                <MenuItem display="flex" gap='2px'> Edit</MenuItem>
+                <MenuItem display="flex" gap='2px'> Delete</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
         </Stack>    
       </Box>
     </Center>
