c4bf1147034d576f18567ce4d28c68db2e80a041
narharikale <naraharikale5051@gmail.com>
2022-06-10 13:51:53 +0530

    added `bookmark` Icons

---
+++ src/frontend/components/icons/BookmarkSM.js
@@ -0,0 +1,13 @@
+export function BookmarkIconSM ({color}) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" 
+            width="18" height="18"
+            viewBox="0 0 24 24" fill="none" stroke={color}
+            strokeWidth="2" 
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-bookmark">
+             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+        </svg>
+    )
+} 
