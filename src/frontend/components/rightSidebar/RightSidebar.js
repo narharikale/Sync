eceec9a6dbb0a911ddad35600bb2bfb1d6991769
narharikale <naraharikale5051@gmail.com>
2022-06-10 13:56:01 +0530

    added like,comment,bookmark,newpost,profilecard

--- src/frontend/components/rightSidebar/RightSidebar.js
+++ src/frontend/components/rightSidebar/RightSidebar.js
@@ -1,9 +1,54 @@
-import { Box , Text , Stack , Avatar } from '@chakra-ui/react'
+import { Box , Text , Stack , Avatar , Button} from '@chakra-ui/react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import { sortPosts } from '../../features/posts/postsSlice';
+
 
 
 function RightSidebar() {
+
+  const { allUsers } = useSelector( (state) => state.user );
+  const dispatch = useDispatch();
+  const location = useLocation();
   return (
     <Box p={4}>
+          { location.pathname === '/network' || location.pathname === '/bookmark' ? 
+            <Box display={'flex'} justifyContent={'space-between'} py={3}>
+              <Button as={Button}
+              variant='outline' 
+              size='sm' 
+              borderColor={'gray.300'}
+              _hover={
+                {
+                  backgroundColor:'green.300',
+                  color:'white'
+                }
+              } 
+              onClick={() => dispatch(sortPosts("latest"))}
+              bg={'white'} > 
+                Get Latest Post 🚀
+              </Button>
+
+              <Button as={Button}
+              variant='outline' 
+              size='sm' 
+              borderColor={'gray.300'}
+              _hover={
+                {
+                  backgroundColor:'green.300',
+                  color:'white'
+                }
+              } 
+              bg={'white'} 
+              onClick={() => dispatch(sortPosts("trending"))}
+              > 
+                Show Trending 🔥
+              </Button>
+            </Box> : " "
+
+          }
+            
+
       <Text
         fontSize={'18px'} 
         borderBottom={'1px'} 
@@ -13,46 +58,35 @@ function RightSidebar() {
         >
          Recommended People
       </Text>
-      
-      <Stack 
-        direction={'row'} 
-        spacing={4} 
-        align={'start'} 
-        p={2}
-        borderRadius={'md'}
-        _hover={{
-          bg: 'gray.50',
-        }}
-        >
-          <Avatar
-            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
-            alt={'Author'}
-          />
-          <Box direction={'column'} spacing={0} fontSize={'sm'} >
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={'gray.500'}> Frontend developer | React | NEXT.js |  </Text>
-          </Box>
-           
-        </Stack>
-        <Stack 
-        direction={'row'} 
-        spacing={4} 
-        align={'start'} 
-        p={2}
-        borderRadius={'md'}
-        _hover={{
-          bg: 'gray.50',
-        }}
-        >
-          <Avatar
-            src={'https://avatars.githubusercontent.com/u/810438?v=4'}
-            alt={'Author'}
-          />
-          <Box direction={'column'} spacing={0} fontSize={'sm'} >
-            <Text fontWeight={600}>Dan Abramov</Text>
-            <Text color={'gray.500'}>Creator | React </Text>
-          </Box>
-        </Stack>  
+      { allUsers.slice(0 ,4).map((user , index) => {
+        
+        return (
+            <Stack 
+            key={index}
+            direction={'row'} 
+            spacing={4} 
+            align={'start'} 
+            p={2}
+            borderRadius={'md'}
+            _hover={{
+              bg: 'gray.50',
+            }}
+            >
+              <Avatar
+                src={user?.avatarURL}
+                alt={'Author'}
+              />
+              <Box direction={'column'} spacing={0} fontSize={'sm'} >
+                  <Text
+                   _hover={{ textDecoration: "underline" , color:'green.400' }} 
+                    as={Link} to={`/profile/${user?.username}`} 
+                    fontWeight={600}>{ user?.firstName } { user?.lastName }
+                  </Text>
+                <Text color={'gray.500'}> { user?.bio } </Text>
+              </Box>
+            </Stack>
+        )
+      })}
     </Box>
   )
 }
